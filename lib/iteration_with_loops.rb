a8fef529d189eee56c22883e6f8dd77d4e05f68d
Iona <ionabrabender@gmail.com>
2020-05-01 23:02:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].length do
       if src[row_index][element_index].class == String
-        sentence.push(src[row_index][element_index])
+        sentence.join(src[row_index][element_index])
       end
     element_index += 1
   end
